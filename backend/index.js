f48ae0e3603d91d59dc5d46bda76c0f3b4721e35
RojaPinnamraju <rojapinnamraju@gmail.com>
2025-05-28 23:43:58 -0700

    Fix require statement to use puppeteer-core

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const cors = require('cors');
 require('dotenv').config();
 
